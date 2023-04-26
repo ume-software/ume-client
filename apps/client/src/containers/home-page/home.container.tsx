73dc89b9211089d9c395d6cff8d19770f173a41a
Võ Văn Quế <97717305+quevv@users.noreply.github.com>
2023-04-26 10:16:22 +0700

    Feat: create category slide and drawer

--- apps/client/src/containers/home-page/home.container.tsx
+++ apps/client/src/containers/home-page/home.container.tsx
@@ -1,21 +1,15 @@
-
-
-import { AppLayout } from '~/components/layouts/app-layout/app-layout'
 import Category from './component/category'
 import Cover from './component/cover'
+
+import { AppLayout } from '~/components/layouts/app-layout/app-layout'
 
 const HomePage = (props) => {
   return (
     <AppLayout {...props}>
-      <div className="flex flex-1">
+      <div className="flex mx-16 flex-col">
         <Cover />
+        <Category />
       </div>
-      <div>
-        <p className='text-white mx-20 my-10 block font-semibold text-3xl '>
-          Danh mục
-        </p>
-      </div>
-
     </AppLayout>
   )
 }
