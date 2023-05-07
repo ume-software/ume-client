c16a6678a097cab72f961799a0f77f802ccc0d8e
huy-raw <ruacon130@gmail.com>
2023-05-07 21:08:05 +0700

    chore: remove unsued code

--- apps/client/src/api/services/booking-service.ts
+++ apps/client/src/api/services/booking-service.ts
@@ -1,13 +1,11 @@
-import { TRPCError } from "@trpc/server"
+import { TRPCError } from '@trpc/server'
 
 export const getListSkill = () => {
-    try {
-
-    } catch (error) {
-        throw new TRPCError({
-            code: 'BAD_REQUEST',
-            message:
-                'Failed to sign in.'
-        })
-    }
+  try {
+  } catch (error) {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+      message: 'Failed to sign in.',
+    })
+  }
 }
