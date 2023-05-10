b8a2cfb0074d9609654ad9fbb14529c1d42f1ead
haihpse150218 <haihpse150218@gmail.com>
2023-05-10 19:28:02 +0700

    fix reponsive card promotion and gap size

--- apps/client/src/containers/home-page/component/promotion.tsx
+++ apps/client/src/containers/home-page/component/promotion.tsx
@@ -8,7 +8,7 @@ export default function Promotion({ listCard }) {
   return (
     <div className="container mx-auto">
       <p className="text-white block font-semibold text-3xl pt-8">Ume</p>
-      <div className="grid lg:grid-cols-4 gap-8 mt-12">
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10 mt-12">
         {listCard.map((card) => (
           <PromoteCard card={card} />
         ))}
