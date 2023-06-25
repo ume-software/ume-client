c89f767c182c301fe1efe6a63700e713c52f5357
Do Tran Minh Chu <dotranminhchu@gmail.com>
2023-06-25 23:22:46 +0700

    [FIX] : live

--- apps/client/src/containers/detail-live-page/detail-live-page.container.tsx
+++ apps/client/src/containers/detail-live-page/detail-live-page.container.tsx
@@ -1,39 +1,43 @@
-import Head from 'next/head'
-import { useEffect, useRef, useState } from 'react'
-import { LiveStreamChannelResponse } from 'ume-steaming-service-openapi'
-import { AppLayout } from '~/components/layouts/app-layout/app-layout'
-import { trpc } from '~/utils/trpc'
+import Head from 'next/head';
+import { useEffect, useRef, useState } from 'react';
+import { LiveStreamChannelResponse } from 'ume-steaming-service-openapi';
+import { AppLayout } from '~/components/layouts/app-layout/app-layout';
+import { trpc } from '~/utils/trpc';
+import flvPlayer from 'flv.js';
+import { getEnv } from '~/env';
 
 const DetailPlayer = (props) => {
- let channel: LiveStreamChannelResponse  | undefined;
- const { slug } = props;
-  const videoRef = useRef<any>(null); 
-   useEffect(() => {
-     if (channel && typeof window !== 'undefined') {
-        const flvPlayer = require('flv.js');
-        const player = flvPlayer.createPlayer({
+  const { slug } = props;
+  const videoRef = useRef<any>(null);
+  const [channel, setChannel] = useState<LiveStreamChannelResponse | undefined>(undefined);
+
+  useEffect(() => {
+    if (channel && typeof window !== 'undefined') {
+      const player = flvPlayer.createPlayer({
         type: 'flv',
-        url: `http://localhost:5301/live/${slug}.flv`,
-        });
-        console.log("player ==> ",player);
-        console.log("videoRef ===> ",videoRef)
-        player.attachMediaElement(videoRef.current);
-        player.load();
+        url: `${getEnv().baseLivestreamFlvURL}/live/${slug}.flv`,
+      });
+      player.attachMediaElement(videoRef.current);
+      player.load();
+
+      return () => {
+        player.unload();
+        player.detachMediaElement();
+      };
     }
-  }, []);
+  }, [channel, slug]);
+
+  const { data: provider, isLoading: loadingProvider } = trpc.useQuery(['streaming.getLiveStreamChannelById', { channelId: slug as string, limit: '1', page: '1' }]);
   
+  useEffect(() => {
+    if (!loadingProvider) {
+      setChannel(provider?.data);
+    }
+  }, [loadingProvider, provider]);
 
-  const {
-    data: provider,
-    isLoading: loadingProvider,
-    isFetching,
-  } = trpc.useQuery(['streaming.getLiveStreamChannelById', {channelId:slug as string,limit:'1',page:'1'}])
   if (loadingProvider) {
-    return <></>
+    return <></>;
   }
-  channel = provider?.data
-
-
 
   return (
     <>
@@ -41,14 +45,15 @@ const DetailPlayer = (props) => {
         <title>UME | Live</title>
       </Head>
       <AppLayout {...props}>
-        <div className='text-white'>
-            <h1>Live</h1>
-            <video ref={videoRef} style={{ width: '80%' }} controls />
-            <h1 >{channel?.title}</h1>
-            <h5>{channel?.description}</h5>
+        <div className="text-white">
+          <h1>Live</h1>
+          <video ref={videoRef} style={{ width: '80%' }} controls />
+          <h1>{channel?.title}</h1>
+          <h5>{channel?.description}</h5>
         </div>
       </AppLayout>
     </>
-  )
-}
-export default DetailPlayer
+  );
+};
+
+export default DetailPlayer;
