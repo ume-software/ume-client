f5d1067d5aba83a77373a69a332d5873fccd0c17
huy-raw <huyqtrieu@kms-technology.com>
2023-12-02 14:04:50 +0700

    feat: config env

--- apps/admin/src/env.ts
+++ apps/admin/src/env.ts
@@ -1,8 +1,9 @@
 export const getEnv = () => {
-  const baseUmeServiceURL = process.env.NEXT_PUBLIC_BASE_BOOKING_API_URL || 'http://localhost:5997'
-  const baseChattingURL = process.env.NEXT_PUBLIC_BASE_CHATTING_API_URL || 'http://localhost:5587'
-  const baseLivestreamURL = process.env.NEXT_PUBLIC_BASE_LIVESTREAM_API_URL || 'http://localhost:5687'
-  const baseUploadFileURL = process.env.NEXT_PUBLIC_BASE_UPLOAD_API_URL || 'http://localhost:5687'
+  const baseUmeServiceURL = process.env.NEXT_PUBLIC_BASE_UME_API_URL || 'https://api.ume.software:8001/ume-service'
+  const baseChattingURL =
+    process.env.NEXT_PUBLIC_BASE_CHATTING_API_URL || 'https://api.ume.software:8002/chatting-service'
+  const baseUploadFileURL =
+    process.env.NEXT_PUBLIC_BASE_UPLOAD_API_URL || 'https://api.ume.software:8000/upload-service'
 
   const baseSocketBookingURL =
     process.env.NEXT_PUBLIC_BASE_BOOKING_SOCKET_URL || 'http://ume.software:8101/booking-service/socket'
@@ -11,7 +12,6 @@ export const getEnv = () => {
   return {
     baseUmeServiceURL,
     baseChattingURL,
-    baseLivestreamURL,
     baseSocketBookingURL,
     baseSocketChattingURL,
     baseUploadFileURL,
