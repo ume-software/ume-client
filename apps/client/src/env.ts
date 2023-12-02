e4a60a6c864170c58e9077064395a187a1f21d6c
huy-raw <huyqtrieu@kms-technology.com>
2023-12-02 15:02:49 +0700

    feat: update env

--- apps/client/src/env.ts
+++ apps/client/src/env.ts
@@ -1,18 +1,15 @@
 export const getEnv = () => {
-  const baseUmeServiceURL = process.env.NEXT_PUBLIC_BASE_BOOKING_API_URL ?? 'http://localhost:5698'
-  const baseChattingURL = process.env.NEXT_PUBLIC_BASE_CHATTING_API_URL ?? 'http://localhost:5587'
-  const baseLivestreamURL = process.env.NEXT_PUBLIC_BASE_LIVESTREAM_API_URL ?? 'http://localhost:5687'
-  const baseUploadServiceURL = process.env.NEXT_PUBLIC_BASE_UPLOAD_SERVICE_API_URL ?? 'http://ume.software:8100'
+  const baseUmeServiceURL = process.env.NEXT_PUBLIC_BASE_UME_API_URL || 'https://api.ume.software:8001'
+  const baseChattingURL = process.env.NEXT_PUBLIC_BASE_CHATTING_API_URL || 'https://api.ume.software:8002'
+  const baseUploadFileURL = process.env.NEXT_PUBLIC_BASE_UPLOAD_API_URL || 'https://api.ume.software:8000'
 
-  const baseSocketBookingURL =
-    process.env.NEXT_PUBLIC_BASE_BOOKING_SOCKET_URL ?? 'http://ume.software:8101/booking-service/socket'
-  const baseSocketChattingURL = process.env.NEXT_PUBLIC_BASE_CHATTING_SOCKET_URL ?? 'http://18.136.124.5:8002'
+  const baseSocketBookingURL = process.env.NEXT_PUBLIC_BASE_UME_SOCKET_URL || 'https://ume.software:8001/'
+  const baseSocketChattingURL = process.env.NEXT_PUBLIC_BASE_CHATTING_SOCKET_URL || 'https://api.ume.software:8002'
   return {
     baseUmeServiceURL,
     baseChattingURL,
-    baseLivestreamURL,
     baseSocketBookingURL,
     baseSocketChattingURL,
-    baseUploadServiceURL,
+    baseUploadFileURL,
   }
 }
