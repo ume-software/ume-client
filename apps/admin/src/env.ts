38724efc0abf449290564f67fc97550962b64064
huy-raw <huyqtrieu@kms-technology.com>
2023-12-02 14:25:42 +0700

    feat: config env

--- apps/admin/src/env.ts
+++ apps/admin/src/env.ts
@@ -6,9 +6,9 @@ export const getEnv = () => {
     process.env.NEXT_PUBLIC_BASE_UPLOAD_API_URL || 'https://api.ume.software:8000/upload-service'
 
   const baseSocketBookingURL =
-    process.env.NEXT_PUBLIC_BASE_BOOKING_SOCKET_URL || 'http://ume.software:8101/booking-service/socket'
+    process.env.NEXT_PUBLIC_BASE_UME_SOCKET_URL || 'https://api.ume.software:8001/ume-service/socket'
   const baseSocketChattingURL =
-    process.env.NEXT_PUBLIC_BASE_CHATTING_SOCKET_URL || 'http://ume.software:8102/chatting-service/socket'
+    process.env.NEXT_PUBLIC_BASE_CHATTING_SOCKET_URL || 'https://api.ume.software:8002/chatting-service/socket'
   return {
     baseUmeServiceURL,
     baseChattingURL,
