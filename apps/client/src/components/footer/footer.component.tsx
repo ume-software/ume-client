4a6eef95a8cacb246f0ebdb1cbc05e894fa9cc45
huy-raw <huyqtrieu@kms-technology.com>
2023-12-17 21:59:01 +0700

    fix fix UI chart

--- apps/client/src/components/footer/footer.component.tsx
+++ apps/client/src/components/footer/footer.component.tsx
@@ -8,7 +8,14 @@ export const Footer = () => {
     <div className="grid w-full grid-cols-12 gap-4 py-8 bg-umeHeader min-h-[10vh]">
       <div className="col-span-4 col-start-2 ">
         <Image width={160} height={40} alt="logo-ume" src={logo} layout="fixed" />
-        <div className="flex flex-col justify-center mx-auto mt-2 text-slate-300"></div>
+        <div className="flex flex-col justify-center mx-auto mt-2 text-slate-300">
+          <div>
+            <h2 className="mb-2 text-xl font-semibold">Thông tin liên hệ</h2>
+            <p>Địa chỉ: 123 Đường ABC, Thành phố XYZ, Quốc gia ABC</p>
+            <p>Email: info@ume.com</p>
+            <p>Điện thoại: (+84) 123 456 789</p>
+          </div>
+        </div>
       </div>
       <div className="col-span-2 text-white">
         <div className="text-xl font-medium">Bắt đầu</div>
